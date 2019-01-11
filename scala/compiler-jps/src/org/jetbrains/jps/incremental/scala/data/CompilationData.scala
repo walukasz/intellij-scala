4649aaf1f8d80d9aab147793d25d2e7f3408f504
Lukasz Wawrzyk <Lukasz.Wawrzyk@morganstanley.com>
2019-01-11 16:26:00 +0100

    Create classpath set for faster lookup

--- scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/data/CompilationData.scala
+++ scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/data/CompilationData.scala
@@ -76,7 +76,8 @@ abstract class BaseCompilationData extends CompilationDataFactory {
         throw new RuntimeException(message)
       })
 
-      val relevantOutputToCacheMap = (outputToCacheMap - output).filter(p => classpath.contains(p._1))
+      val classpathSet = classpath.toSet
+      val relevantOutputToCacheMap = (outputToCacheMap - output).filter(p => classpathSet.contains(p._1))
 
       val outputGroups = createOutputGroups(chunk)
 
