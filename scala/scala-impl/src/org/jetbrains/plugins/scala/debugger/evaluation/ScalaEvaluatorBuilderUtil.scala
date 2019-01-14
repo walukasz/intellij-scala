8d4cfb9e6f7331a6d605542b7c11b693f9ab8128
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-01-14 17:22:28 +0100

    ScDoStmt -> ScDo
    SCL-14809

--- scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaEvaluatorBuilderUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaEvaluatorBuilderUtil.scala
@@ -1103,7 +1103,7 @@ private[evaluation] trait ScalaEvaluatorBuilderUtil {
     new WhileStatementEvaluator(condEvaluator, iterationEvaluator, null)
   }
 
-  def doStmtEvaluator(doSt: ScDoStmt): Evaluator = {
+  def doStmtEvaluator(doSt: ScDo): Evaluator = {
     val condEvaluator = doSt.condition match {
       case Some(cond) => evaluatorFor(cond)
       case None =>
