e982b3695a945a73586d3069adc033c55f7f7633
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-01-14 17:55:12 +0100

    ScWhile: properties renamed
    SCL-14809

--- scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaEvaluatorBuilderUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaEvaluatorBuilderUtil.scala
@@ -1095,7 +1095,7 @@ private[evaluation] trait ScalaEvaluatorBuilderUtil {
       case Some(cond) => evaluatorFor(cond)
       case None => throw EvaluationException(ScalaBundle.message("while.statement.without.condition"))
     }
-    val iterationEvaluator = ws.body match {
+    val iterationEvaluator = ws.expression match {
       case Some(body) => evaluatorFor(body)
       case None => throw EvaluationException(ScalaBundle.message("while.statement.without.body"))
     }
